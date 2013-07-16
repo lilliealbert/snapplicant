7819bbb9c7e9771429451de5b130d28c91f623f2
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-16 16:54:26 -0700

    Initial commit with postgres replacing sqlite3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Snapplicator::Application.config.secret_token = '42d16488b873d4f983b0571e57700d503652ce04d1ea54f8d71e71d79851de636c4591d1ee0a6255f3e0ebc5b41cc1fd5d7ade4171f7d14b06e42b4cec25c8e4'
