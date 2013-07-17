9ef37e683db406efd78657972bf0ba987d6fd11c
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-16 17:12:15 -0700

    Add devise for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Snapplicator::Application.routes.draw do
+
+  devise_for :users
+
+  root :to => 'users#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,10 +50,6 @@ Snapplicator::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
