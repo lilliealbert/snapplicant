8d7506ea733f84e89bc28b2f3759b2a930ef4db6
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-17 13:39:37 -0700

    Show jobs by status on jobs index - Not thrilled with the amount of logic in the view - ripe for refactoring!

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,4 +3,8 @@ class Job < ActiveRecord::Base
   validates :title, :company, presence: true
 
   attr_accessible :title, :company, :source_url, :description, :notes
+
+  def is_status?(a_status)
+    self.status == a_status
+  end
 end
