3db71796a414b24944e5d854707c8cda925cde41
Lillie Chilen <lillie.chilen@gmail.com>
2013-07-17 16:45:40 -0700

    Add Markdown parsing via RedCarpet gem for job descriptions and notes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {autolink: true, lax_spacing: true})
+    markdown.render(text).html_safe
+  end
 end
